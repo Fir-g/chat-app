099cca2383d28d3c8c859522c1f6e560b7eaaaec
Firoj Ahamad <firojahamad5072002@gmail.com>
2024-03-26 06:22:01 +0530

    Deployment

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,24 +1,29 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
+
 export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
+
         if (password !== confirmPassword) {
             return res.status(400).json({ error: "Passwords don't match" });
         }
+
         const user = await User.findOne({ username });
 
         if (user) {
             return res.status(400).json({ error: "Username already exists" });
         }
-        //HASH PASSWORD HERE
+
+        // HASH PASSWORD HERE
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        //https://avatar-placeholder.iran.liara.run/
 
-        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+        // https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
         const newUser = new User({
             fullName,
@@ -28,10 +33,8 @@ export const signup = async (req, res) => {
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
         });
 
-
-
         if (newUser) {
-            //Generate JWT token here
+            // Generate JWT token here
             generateTokenAndSetCookie(newUser._id, res);
             await newUser.save();
 
@@ -46,8 +49,7 @@ export const signup = async (req, res) => {
         }
     } catch (error) {
         console.log("Error in signup controller", error.message);
-        res.status(500).json({ error: "internal Server Error" });
-
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,14 @@
 import User from "../models/user.model.js";
 
-export const getUsersForSidebar=async (req,res)=>{
+export const getUsersForSidebar = async (req, res) => {
     try {
-        const loggedInUserId =req.user._id;
-       // const allUsers=await User.find().select("-password");// if u require to send message yourself
-        const filteredUsers= await User.find({_id:{$ne:loggedInUserId}}).select("-password");//everyuser other than yourself
+        const loggedInUserId = req.user._id;
+
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
         res.status(200).json(filteredUsers);
-
     } catch (error) {
-        console.error("Error in getUserForSidebar: ",error.message);
-        res.status(500).json({error: "Internal server error"});
-        
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
